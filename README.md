710befbf756b42f282e8114d1a42e0be6b83dde1
Cesar Daniel Meneses <cdanielmg200@gmail.com>
2022-07-26 21:38:16 -0500

    Se actualiza build.gradle para hacer uso de la version 7.1 de gradle. Por lo cual tambien se actualizo: README.md y los archivos relacionados con gradle wrapper. Tambien se actualizaron las versiones de las dependencias lo que ocasiono la modificacion de varios imports en las clases GitHubTestRunner.java y CreateRepositoryInGitHubStepDefinition.java

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ La estructura completa del proyecto es la siguiente:
 
 ## Requerimientos
 
-Para correr el proyecto se necesita Java JDK 1.8 y Gradle preferiblemente con la versión 4.8.1.
+Para correr el proyecto se necesita Java JDK 1.8 y Gradle preferiblemente con la versión 7.1.
 
 ## Para correr el proyecto
 
@@ -111,7 +111,7 @@ The complete structure of the project is the following:
 ````
 ## Requirements
 
-To run the project you need Java JDK 1.8 and Gradle preferably with version 4.3.
+To run the project you need Java JDK 1.8 and Gradle preferably with version 7.1.
 
 ## To run the project
 
--- src/test/java/co/com/yourcompany/certification/nameproject/runners/GitHubTestRunner.java
+++ src/test/java/co/com/yourcompany/certification/nameproject/runners/GitHubTestRunner.java
@@ -1,14 +1,13 @@
 package co.com.yourcompany.certification.nameproject.runners;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.SnippetType;
+import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features = "src/test/resources/features/create_repository.feature",
         glue = {"co.com.yourcompany.certification.nameproject.stepdefinitions"},
-        snippets = SnippetType.CAMELCASE)
+        snippets = CucumberOptions.SnippetType.CAMELCASE)
 public class GitHubTestRunner {
 
 }
